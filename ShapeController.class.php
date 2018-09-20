e4fb6c344de9ce81c334c6beb5d415587b99fd2b
Eugen <glazov.it@gmail.com>
2018-09-20 15:22:25 +0400

    start

---
+++ ShapeController.class.php
@@ -0,0 +1,27 @@
+<?php
+
+/**
+ * Контроллер карточки фигуры
+ */
+class ShapeController extends Controller
+{
+    public $view = 'v_shape';
+
+    public $title = 'shape card';
+
+
+
+    public function viewCard ()
+    {
+        $result = [];
+        return $result;
+    }
+    
+    public function add()
+    {
+        $result = [];
+        return $result;
+    }
+
+
+}
